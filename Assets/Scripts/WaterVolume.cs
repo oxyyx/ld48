9d8442af18aced66aab277de2253c27621da0072
B Bergfeld <b.bergfeld@hotmail.com>
2021-04-26 12:41:31 +0200

    Adds draining/filling to watervolumes

--- Assets/Scripts/WaterVolume.cs
+++ Assets/Scripts/WaterVolume.cs
@@ -1,13 +1,10 @@
 using System.Collections.Generic;
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class WaterVolume : MonoBehaviour
 {
-	[SerializeField]
-	private float gravityScale = 0.25f;
-	[SerializeField]
-	private float linearDrag = 1;
-
 	private class RigidBodyConfig {
 		public float GravityScale { get; set; }
 		public float LinearDrag { get; set; }
@@ -27,12 +24,45 @@ public class WaterVolume : MonoBehaviour
 		}
 	}
 
+	[SerializeField]
+	private float gravityScale = 0.25f;
+	[SerializeField]
+	private float linearDrag = 1;
+
+	[SerializeField]
+	private bool startsEmpty = false;
+	[SerializeField]
+	private float fillingSpeed = 1.0f;
+	[SerializeField]
+	private WaterVolume[] drainageTargets = new WaterVolume[0];
+	[SerializeField]
+	private float drainingSpeed = 1.0f;
+
+	private float halfSpriteHeight;
+	private float bottomY;
+	private float originalScale;
+	private bool isEmpty;
+	private bool isFull;	
+
 	private RigidBodyConfig underwaterConfig;
 	private Dictionary<GameObject, RigidBodyConfig> storedConfigs;
 
 	private void Awake() {
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		halfSpriteHeight = renderer.sprite.bounds.max.y;
+		bottomY = renderer.bounds.min.y;
+		originalScale = transform.localScale.y;
+		isEmpty = false;
+		isFull = true;
+
 		underwaterConfig = new RigidBodyConfig { GravityScale = gravityScale, LinearDrag = linearDrag };
 		storedConfigs = new Dictionary<GameObject, RigidBodyConfig>();
+
+		if(startsEmpty) {
+			isEmpty = true;
+			isFull = false;
+			InitializeAsEmpty();
+		}
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision) {
@@ -57,6 +87,53 @@ public class WaterVolume : MonoBehaviour
 		}
 	}
 
+	public void Drain() {
+		if(!isFull) {
+			return;
+		}
+
+		StartCoroutine(DrainCoroutine());
+		foreach(WaterVolume drainageTarget in drainageTargets){
+			if(drainageTarget != null) {
+				drainageTarget.Fill();
+			}
+		}		
+	}	
+
+	public void Fill() {
+		if(!isEmpty) {
+			return;
+		}
+
+		StartCoroutine(FillCoroutine());
+	}
+
+	private IEnumerator DrainCoroutine() {
+		isFull = false;
+
+		const float drainingSpeedModifier = 0.1f;
+		while(transform.localScale.y - (drainingSpeed * drainingSpeedModifier) > 0) {
+			UpdateTransformForScale(transform.localScale.y - (drainingSpeed * drainingSpeedModifier));
+			yield return null;
+		}
+
+		UpdateTransformForScale(0);
+		isEmpty = true;
+	}
+
+	private IEnumerator FillCoroutine() {
+		isEmpty = false;
+
+		const float fillingSpeedModifier = 0.1f;
+		while(transform.localScale.y + (fillingSpeed * fillingSpeedModifier) < originalScale) {
+			UpdateTransformForScale(transform.localScale.y + (fillingSpeed * fillingSpeedModifier));
+			yield return null;
+		}
+
+		UpdateTransformForScale(originalScale);
+		isFull = true;
+	}
+
 	private void StoreRigidBodyConfig(Rigidbody2D rigidBody) {
 		if(storedConfigs.ContainsKey(rigidBody.gameObject)) {
 			return;
@@ -64,5 +141,16 @@ public class WaterVolume : MonoBehaviour
 
 		RigidBodyConfig config = new RigidBodyConfig(rigidBody);
 		storedConfigs.Add(rigidBody.gameObject, config);
+	}
+
+	private void InitializeAsEmpty() {
+		transform.localScale = new Vector3(transform.localScale.x, 0, transform.localScale.z);
+		transform.position = new Vector2(transform.position.x, bottomY);
+	}
+
+	private void UpdateTransformForScale(float scale) {
+		transform.localScale = new Vector3(transform.localScale.x, scale, transform.localScale.z);
+		float positionY = bottomY + (scale * halfSpriteHeight);
+		transform.position = new Vector2(transform.position.x, positionY);
 	}
 }
